8a6dbba2fe97805c9e60fb4a5ef7ab245ca0e8fd
Rob Ellis <kazoomer@gmail.com>
2011-04-01 11:04:20 -0700

    Added test for gaps in regex tokenizer

--- test/test-token-regexp.js
+++ test/test-token-regexp.js
@@ -5,12 +5,10 @@ var wordTok = new tokenizer.WordTokenizer()
 exports.wordToken = function(test) {
 	test.expect(2);
 	var tok = wordTok.tokenize("This is all a dream.");
-
 	test.equal(5,tok.length,"This is all a dream. - should have 5 words");
 
 	// Quotes removed
 	var tok = wordTok.tokenize("She said 'hello'");
-
 	test.equal(3,tok.length,"She said 'hello'. - should have 3 words");
 	test.done();
 	
@@ -20,20 +18,26 @@ var wordPunct = new tokenizer.WordPunctTokenizer();
 exports.wordPunctToken = function(test) {
 	var tok = wordPunct.tokenize("She said 'hello'!");
 	test.expect(1);
-	test.equal(6,tok.length,'She said "hello". - should have 6 words');
+	test.equal(6,tok.length,'She said "hello". - should have 6 tokens');
 	test.done();
 }
 
 
-var regexpTok = new tokenizer.RegexpTokenizer({pattern:/AAA/});
+// Split on Matches
+var regexpTok1 = new tokenizer.RegexpTokenizer({pattern:/someword/g,gaps:false});
 exports.regexpToken = function(test) {
 	test.expect(1);
-	var tok = regexpTok.tokenize("somewordAAAsomewordAAAsomeword");
+	var tok = regexpTok1.tokenize("somewordAAAsomewordAAAsomeword");
 	test.equal(3,tok.length,'somewordAAAsomewordAAAsomeword - should have 3 words');	
 	test.done();
 }
 
+// Split on GAPS
+var regexpTok2 = new tokenizer.RegexpTokenizer({pattern:/AAA/});
 exports.regexpGapsToken = function(test) {
-	test.expect(0);
+	test.expect(1);
+	var tok = regexpTok2.tokenize("somewordAAAsomewordAAAsomeword");
+	test.equal(3,tok.length,'somewordAAAsomewordAAAsomeword - should have 3 words');	
+	
 	test.done();
 }
