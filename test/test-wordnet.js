eb3d27c3fb96ac553a4046fc8c67aa30a8186edd
Rob Ellis <kazoomer@gmail.com>
2011-05-08 11:27:57 -0700

    First pass at porting wordnet

---
+++ test/test-wordnet.js
@@ -0,0 +1,35 @@
+
+var WordNetCorpusReader = require('../lib/wordnet/wordReader');
+
+
+new WordNetCorpusReader({root:'./wordnet'},function(wn){
+    
+  // wn.lemma('zap.v.03.nuke', function(L){
+  //  console.log(L.derivationallyRelatedForms())
+  // });    
+
+  
+  wn.synset('go.v.21', function(move_synset){
+    console.log('getting a synset for go');
+    console.log(move_synset.name, move_synset.pos, move_synset.lexname);
+    console.log(move_synset.lemma_names);
+  
+    // TODO add these in
+    console.log(move_synset.definition);
+    console.log(move_synset.examples);          
+  });
+  
+  // console.log("Navigations:");
+  // wn.synset('travel.v.01',function(S){
+  //   console.log(S.hypernyms())
+  // });
+  
+  // wn.synset('travel.v.02',function(S){
+  //   console.log(S.hypernyms())
+  // });
+  // 
+  // wn.synset('travel.v.03',function(S){
+  //   console.log(S.hypernyms())
+  // });
+  
+});
